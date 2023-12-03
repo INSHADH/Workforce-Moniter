d67e14bd8a619f4a1befe79748d2651218add5ac
INSHADH <inshadh.office@gmail.com>
2023-12-04 04:32:08 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Workforce_Monitor.Models;
+
+namespace Workforce_Monitor.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private WorkforceMonitorDBEntities1 db = new WorkforceMonitorDBEntities1();
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(db.TNEmpMasterTables.ToList());
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TNEmpMasterTable tNEmpMasterTable = db.TNEmpMasterTables.Find(id);
+            if (tNEmpMasterTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tNEmpMasterTable);
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "emp_id,emp_code,emp_name,designation,salary")] TNEmpMasterTable tNEmpMasterTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TNEmpMasterTables.Add(tNEmpMasterTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tNEmpMasterTable);
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TNEmpMasterTable tNEmpMasterTable = db.TNEmpMasterTables.Find(id);
+            if (tNEmpMasterTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tNEmpMasterTable);
+        }
+
+        // POST: Employee/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "emp_id,emp_code,emp_name,designation,salary")] TNEmpMasterTable tNEmpMasterTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tNEmpMasterTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tNEmpMasterTable);
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TNEmpMasterTable tNEmpMasterTable = db.TNEmpMasterTables.Find(id);
+            if (tNEmpMasterTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tNEmpMasterTable);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TNEmpMasterTable tNEmpMasterTable = db.TNEmpMasterTables.Find(id);
+            db.TNEmpMasterTables.Remove(tNEmpMasterTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Workforce_Monitor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Workforce_Monitor")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c88f9bd8-d736-4d2b-8e1b-0b7f8eac8163")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
